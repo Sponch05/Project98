a476cd170c46c676169ace4957d1ba4f00138ff7
Siyammukela <siyammukela@gmail.com>
2017-08-27 00:08:08 +0200

    Added few touch ups to the app

---
+++ app/src/main/java/za/co/sponchware/www/connectedyouth/ProvideFundingActivity.java
@@ -0,0 +1,13 @@
+package za.co.sponchware.www.connectedyouth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProvideFundingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_provide_funding);
+    }
+}
