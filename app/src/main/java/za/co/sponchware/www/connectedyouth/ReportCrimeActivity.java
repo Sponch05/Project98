907d1009ac85777ac13ee4635f9688640b09c1a4
Siyammukela <siyammukela@gmail.com>
2017-08-26 17:24:59 +0200

    Added Report Crime Activity & Blank Crowd Page

--- app/src/main/java/za/co/sponchware/www/connectedyouth/ReportCrimeActivity.java
+++ app/src/main/java/za/co/sponchware/www/connectedyouth/ReportCrimeActivity.java
@@ -3,14 +3,11 @@ package za.co.sponchware.www.connectedyouth;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LandingActivity extends AppCompatActivity
-{
+public class ReportCrimeActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
-
+        setContentView(R.layout.activity_report_crime);
     }
 }
