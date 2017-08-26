417a392fe3faa653d07ec9282cdbae18c64f39d8
sirMothupii <iamkeenan94@gmail.com>
2017-08-26 18:36:55 +0200

    ReAdded the classes

---
+++ app/src/main/java/za/co/sponchware/www/connectedyouth/MainActivity.java
@@ -0,0 +1,10 @@
+package za.co.sponchware.www.connectedyouth;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mothupii_ on 2017-08-26.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
