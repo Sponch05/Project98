e80509486f7b322be37454545e3004bebdf21f24
Siyammukela <siyammukela@gmail.com>
2017-08-27 05:16:38 +0200

    adjusted activity

--- app/src/main/java/za/co/sponchware/www/connectedyouth/get_involved.java
+++ app/src/main/java/za/co/sponchware/www/connectedyouth/get_involved.java
@@ -1,7 +1,9 @@
 package za.co.sponchware.www.connectedyouth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class get_involved extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class get_involved extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_involved);
     }
+
+    public void crimePBtn(View view)
+    {
+        Intent intent = new Intent(this, ReportCrimeActivity.class);
+        startActivity(intent);
+    }
+
+    public void sFunding(View view)
+    {
+        Intent intent = new Intent(this, CrowdFundingActivity.class);
+        startActivity(intent);
+    }
+
 }
