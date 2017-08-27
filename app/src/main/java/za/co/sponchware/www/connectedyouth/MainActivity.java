fbdd44b426bc19c3c09a1042b109a9a2afa074a0
Siyammukela <siyammukela@gmail.com>
2017-08-27 04:17:40 +0200

    added buttons for landing page

--- app/src/main/java/za/co/sponchware/www/connectedyouth/MainActivity.java
+++ app/src/main/java/za/co/sponchware/www/connectedyouth/MainActivity.java
@@ -1,10 +1,40 @@
 package za.co.sponchware.www.connectedyouth;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import static za.co.sponchware.www.connectedyouth.R.layout.activity_main__social;
 
 /**
  * Created by Mothupii_ on 2017-08-26.
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+
+    public void getInvolvedBtnn(View view) {
+
+        Intent intent = new Intent(this, get_involved.class);
+        startActivity(intent);
+    }
+
+    public void learnBtnS(View view) {
+        // Intent intent = new Intent(this, )
+    }
+
+    public void socialBtn(View view)
+    {
+        Intent intent = new Intent(this, MainActivity_Social.class);
+        startActivity(intent);
+
+    }
+
+
 }
